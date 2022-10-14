a33b5784c9e36cd351dbb6d40a8a1637ae161019
Ronald Richie Huang <84059388+rochie55@users.noreply.github.com>
2022-10-14 18:14:56 +0700

    Add files via upload

---
+++ Asg1/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'db_webprog';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
